cb42cb423b6cb02d48eca8cdd0f29d0b4d99b168
khoatrankai <55798758+khoatrankai@users.noreply.github.com>
2023-08-26 16:55:20 +0700

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config({ path: '.env' })
 const connectDB = async()=>{
     try{
-        const con = await mongoose.connect(process.env.MG_ATLAS,{
+        const con = await mongoose.connect('mongodb+srv://trantankhoa2802:trA2NY8YCJoG8oRY@cluster0.ebcrhvb.mongodb.net/?retryWrites=true&w=majority',{
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
